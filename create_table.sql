f28f0882c51f7d4b4eb42f3954a2de8ededbe722
Stat-Git <46615444+StatTeam@users.noreply.github.com>
2023-08-22 08:32:12 -0500

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,6 @@
+create table courses_tbl (course_name varchar2(30), course_number number, location varchar2(30));
+insert into courses_tbl values ('Mathematic - Base I', 1, 'Panama City');
+insert into courses_tbl values ('Mathematic - Base II', 2, 'Panama City');
+insert into courses_tbl values ('Java Basic Concepts', 3, 'Panama Oeste');
+insert into courses_tbl values ('Java Fundamental', 4, 'Panama Oeste');
+commit;
